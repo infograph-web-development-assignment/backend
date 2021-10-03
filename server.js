ce03118ecc8fc8a60d8330fdae8a80dde7a9ef81
FIRAS AHMAD HASAN <x.firashasan@gmail.com>
2021-10-03 12:24:02 +0300

    save and send back fund form to frontend

--- server.js
+++ server.js
@@ -1,92 +1,84 @@
-const express = require('express');
+/** @format */
+
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const routesUrls = require('./routes/routes');
-const cors = require('cors');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const routesUrls = require("./routes/routes");
+const cors = require("cors");
 dotenv.config();
 const Port = process.env.PORT;
 // connecting mongodb (Atlas)
-mongoose.connect(process.env.DATABASE_ACCESS,()=>console.log("database is connected"))
+mongoose.connect(process.env.DATABASE_ACCESS, () =>
+  console.log("database is connected")
+);
 
 app.use(express.json());
 app.use(cors());
 
 //to append routeUls to base path (/app)
-app.use('/app', routesUrls)
+app.use("/app", routesUrls);
 
 // make sure server running on right port number
-app.listen(3004,console.log(`app listening on port: ${3004}`));
+app.listen(3004, console.log(`app listening on port: ${3004}`));
 
+const MyProjectOwnerSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
 
+const MyProjectOwnerModel = mongoose.model(
+  "projectOwner",
+  MyProjectOwnerSchema
+);
 
+app.post("/savePojectOwner", savePojectOwnerHandler);
+app.get("/getSavedProjectOwners", getSavedProjectOwnersHandler);
 
-
-
-
-
-
-
-
-
-// making schema for the data
-const MyDigiSchema = new mongoose.Schema({
-    name: String,
-    level:String,
-    img:String
+function savePojectOwnerHandler(req, res) {
+  const { username, password } = req.body;
+  const saver = new MyProjectOwnerModel({
+    username: username,
+    password: password,
   });
-
-  //creating a model
-const MyDigiModel = mongoose.model('infograph',MyDigiSchema);
-
-
-app.get('/',(req,res)=>{
-    res.send("sever is alive");
-})
-
-//get the path for adding to database function
-app.post('/addToFav', addToFavHandler);
-
-//path for getting data from database function
-app.get('/getFavoritedData',getFavoritedDataHandler);
-
-// get path and info for deleting from database
-app.delete('/deleteFromFavs/:id',deleteFromFavsHandler);
-
-
-// This function is to add the data in database
-function addToFavHandler(req,res)
-{
-    const {name,level,img} = req.body;
-    const saver = new MyDigiModel({
-        name:name,
-        level:level,
-        img:img
-    })
-    console.log(saver);
-    saver.save()
-
+  console.log(saver);
+  saver.save();
 }
 
-// Function to find all saved data in database and send it back to frontend
-function getFavoritedDataHandler(req,res)
-{
-    MyDigiModel.find({},(err,data)=>{
+function getSavedProjectOwnersHandler(req, res) {
+  MyProjectOwnerModel.find({}, (err, data) => {
+    res.send(data);
+  });
+}
+
+// Schema for fund request
+const MyProjectFormSchema = new mongoose.Schema({
+  sector: String,
+  projectName: String,
+  description: String,
+  status:String
+});
+
+const MyProjectFormModel = mongoose.model("projectForm", MyProjectFormSchema);
+app.post("/sendTheFund", sendTheFundHandler);
+app.get('/getapResult',getapResultHandler);
+
+function sendTheFundHandler(req, res) {
+  const { sector, projectName, description,status } = req.body;
+  const saver = new MyProjectFormModel({
+    sector: sector,
+    projectName: projectName,
+    description: description,
+    status:status
+  });
+  console.log(saver);
+  saver.save();
+}
+
+
+  function getapResultHandler(req,res)
+  {
+    MyProjectFormModel.find({}, (err, data) => {
         res.send(data);
-    })
-}
-
-
-//function deletes frpom database and sends back all other saved data
-function deleteFromFavsHandler(req,res)
-{
-    const {id} = req.params;
-    //To remove data from database depending on the id
-    MyDigiModel.remove({_id:id},(err,data)=>{
-        //To find and send back all other saved data after deleting the specific item
-        MyDigiModel.find({},(err,data)=>{
-            res.send(data);
-        })
-    })
-}
-
+      });
+  }
